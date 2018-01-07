4bd963e04113a7f8b26ef177a584f80428afc096
Ayush Jain <ayush.jain36@gmail.com>
2018-01-07 17:19:08 +0100

    Update router.js
    Route for the game page

--- routes/router.js
+++ routes/router.js
@@ -150,6 +150,25 @@ router.get('/profile', function (req, res, next) {
         });
 });
 
+router.get('/game', function (req, res, next) {
+    User.findById(req.session.userId)
+        .exec(function (error, user) {
+            if (error) {
+                return next(error);
+            } else {
+                if (user === null) {
+                    var err = new Error('Not authorized! Go back!');
+                    err.status = 400;
+                    return next(err);
+                } else {
+                    res.sendFile(path.join(__dirname+'./../public/game.html'));
+                }
+            }
+        });
+});
+
+
+
 // GET for logout logout
 router.post('/logout', function (req, res, next) {
     if (req.session) {
