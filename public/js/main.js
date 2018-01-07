e1f456b88bcc98b688a48a8c19f5a10f42802ccf
veeramallirajesh <33874300+veeramallirajesh@users.noreply.github.com>
2018-01-07 18:14:26 +0100

    Updating main.js
    To increment and decrement the dice board counters.
    To drag and drop the coin on the board based on the shell count

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,13 @@ app.controller("gameboard", ["$scope", function ($scope) {
     $scope.nextAttemptChance = [0, 1, 4, 6, 8];
     $scope.playDisabled = false;
     $scope.randowSelectedNumber = 0;
+    $scope.zeroCount = 0;
+    $scope.oneCount = 0;
+    $scope.fourCount = 0;
+    $scope.sixCount = 0;
+    $scope.eightCount = 0;
+    $scope.otherNumber = 0;
+    $scope.canCoinDropArray = [];
     
     (function () {
         $.getJSON("data/moves.json", function(data) {
@@ -55,6 +62,56 @@ app.controller("gameboard", ["$scope", function ($scope) {
         
         $scope.incrementScoreBoard(randomNumber);
     };
+	$scope.incrementScoreBoard = function (caseStr) {
+        console.log("Entered updateScoreBoardFunction : ",caseStr);
+        $scope.randowSelectedNumber = caseStr;
+        switch (caseStr) {
+            case 0:
+                $scope.zeroCount++;
+                break;
+            case 1:
+                $scope.oneCount++;
+                break;
+            case 4:
+                $scope.fourCount++;
+                break;
+            case 6:
+                $scope.sixCount++;
+                break;
+            case 8:
+                $scope.eightCount++;
+                break;
+            default:
+                $scope.otherNumber = caseStr;
+                $scope.playDisabled = true;
+                break;
+        }
+		$scope.count.push(caseStr);
+    };
+	
+	$scope.decrementScoreBoard = function (caseStr) {
+        console.log("Entered updateScoreBoardFunction : ",caseStr);
+        $scope.randowSelectedNumber = caseStr;
+		switch (caseStr) {
+            case 0:
+                $scope.zeroCount--;
+                break;
+            case 1:
+                $scope.oneCount--;
+                break;
+            case 4:
+                $scope.fourCount--;
+                break;
+            case 6:
+                $scope.sixCount--;
+                break;
+            case 8:
+                $scope.eightCount--;
+                break;
+            default:
+                $scope.otherNumber = 0;
+                break;
+        }
     
     	var index = $scope.count.indexOf(caseStr);
 		if (index > -1) {
@@ -64,3 +121,41 @@ app.controller("gameboard", ["$scope", function ($scope) {
 		if($scope.count.length == 0) {
 			$scope.playDisabled = false;
 		}
+	
+$scope.allowDrop = function(ev){
+		ev.preventDefault();
+    };
+	$scope.drag = function(ev){
+		ev.dataTransfer.setData("text", $(ev.target).parent("span.playerCoinContainer").attr("id"));
+		ev.dataTransfer.setData("playerText", $(ev.target).parent("span.playerCoinContainer").attr("player"))
+    };
+	$scope.drop = function(ev){
+		ev.preventDefault();
+		console.log("Value of dropped area is : ",$(ev.target).hasClass(".canDrop"));
+		if($(ev.target).hasClass("canDrop")) {
+			console.log("Entered into condition");
+			var coinId = ev.dataTransfer.getData("text");
+			var playerText = ev.dataTransfer.getData("playerText")
+			$(ev.target).append($("#"+coinId+"[player='"+playerText+"']"));
+			$("#"+coinId+"[player='"+playerText+"']").attr("currentposition", $(ev.target).parents(".gameCellBoxes").attr("position"));
+			var result = $.grep($scope.canCoinDropArray, function(e){ 
+				return e.position == $(ev.target).parents(".gameCellBoxes").attr("position"); 
+			});
+			$scope.decrementScoreBoard(result[0].strikeValue);
+		}
+    };
+}]);
+
+function allowDrop(ev) {
+    ev.preventDefault();
+	angular.element(document.getElementById("gameContainer")).scope().allowDrop(ev);
+}
+
+function drag(ev) {
+	angular.element(document.getElementById("gameContainer")).scope().drag(ev);
+}
+
+function drop(ev) {
+	ev.preventDefault();
+	angular.element(document.getElementById("gameContainer")).scope().drop(ev);
+}
